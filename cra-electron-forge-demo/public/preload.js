36a601412399e651cddfef3f77b93cffde699395
br3nr <repetitive_routine@protonmail.com>
2022-08-15 18:10:11 +0800

    Working packaging with rebuilt code

--- cra-electron-forge-demo/public/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-// OpenFILE dialog is our invocation endpoint
-contextBridge.exposeInMainWorld('electronAPI', {
-  getPorts: () => ipcRenderer.invoke('dialog:getPorts'),
-  openPort: (com_path) => ipcRenderer.send('dialog:openPort', com_path),
-  openCom: () =>  ipcRenderer.invoke('dialog:openCom'),
-  sendMessage: (message) => ipcRenderer.send('dialog:sendMessage', message)
-})
-
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,14 +7,14 @@ import {HashRouter, Routes, Route} from "react-router-dom";
 import PortView from "./pages/PortView";
 
 
-ReactDOM.render(
-    <HashRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <HashRouter>
       <Routes>
         <Route path="/" element={<App />}/>
         <Route path="ports" element= {<PortView />}/>
       </Routes>
-    </HashRouter>,
-  document.getElementById('root')
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
