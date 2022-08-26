04cc17e2da81713fa6f7210b01109f9480cb6dde
macx <repetitive_routine@protonmail.com>
2022-08-26 15:12:31 +0800

    line chart page dark theme

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ToggleDarkMode from './components/ToggleDarkMode'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import NavBar from './components/NavBar'
+
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
